db04e4ebca3f7892a89ca38a48a08c2098f6e3c8
Jonathan Torres <jonathantorres41@gmail.com>
2012-01-16 23:30:09 -0400

    Added game over logic & game over/restart screen.

--- js/ShootSquares.js
+++ js/ShootSquares.js
@@ -18,7 +18,10 @@ ShootSquares.Main = new function() {
 	var scoreText;
 	var lifeText;
 	var gameScore;
+	var gameState;
 	var playerLife;
+	var gameOverText;
+	var restartGameText;
 
 	this.init = function() {
 		canvas = document.getElementById('main');
@@ -30,22 +33,12 @@ ShootSquares.Main = new function() {
 		Ticker.setFPS(30);
 		Ticker.addListener(stage);
 		
-		//startMenu();
-		
-		//other game settings
-		numOfSquaresX = 79;
-		numOfSquaresY = 2;
-		moveLeft = false;
-		moveRight = false;
-		squares = new Array();
-		bullets = new Array();
-		gameScore = 0;
-		playerLife = 100;
+		gameState = 'menu'
 		
 		$(window).resize(onResizeWindow);
 		stage.tick = onStageTick;
 		
-		startGame();
+		startMenu();
 		
 		stage.update();
 	}
@@ -71,6 +64,8 @@ ShootSquares.Main = new function() {
 	function onDocumentKeyUp(e) {
 		if (e.keyCode == 32) {
 			stage.removeChild(gametitle, gameinstructions);
+			gametitle = null;
+			gameinstructions = null;
 			stage.update();
 			
 			startGame();
@@ -79,6 +74,16 @@ ShootSquares.Main = new function() {
 	}
 	
 	function startGame() {
+		//some game settings
+		numOfSquaresX = 79;
+		numOfSquaresY = 2;
+		moveLeft = false;
+		moveRight = false;
+		squares = new Array();
+		bullets = new Array();
+		gameScore = 0;
+		playerLife = 100;
+		
 		//create the score info
 		scoreText = new Text('Score: ' + gameScore + 'pts', '10px Arial', '#999');
 		scoreText.textAlign = 'left';
@@ -123,6 +128,9 @@ ShootSquares.Main = new function() {
 				stage.addChild(square);
 			}
 		}
+		
+		//change state
+		gameState = 'play';
 		
 		//keyboard events
 		$(document).bind('keydown.triangle', keyDownTriangle);
@@ -170,99 +178,158 @@ ShootSquares.Main = new function() {
 		}
 	}
 	
+	function gameOver() {
+		gameState = 'menu';
+		
+		stage.removeChild(triangle, scoreText, lifeText);
+		
+		/* remove any remaining squares */
+		if (squares.length != 0) {
+			for (var i = squares.length; i >= 0; i--) {
+				stage.removeChild(squares[i]);
+				squares.splice(i, 1);
+			}
+		}
+		
+		//clean up objects and events
+		triangle = null;
+		scoreText = null;
+		lifeText = null;
+		squares = null;
+		bullets = null;
+		$(document).unbind('keydown.triangle', keyDownTriangle);
+		$(document).unbind('keyup.triangle', keyUpTriangle);
+		
+		gameOverText = new Text('Game Over!','20px Arial', '#FFF');
+		gameOverText.textAlign = 'center';
+		gameOverText.x = canvas.width * 0.5;
+		gameOverText.y = canvas.height * 0.5;
+		
+		restartGameText = new Text('Press spacebar to restart the game', '10px Arial', '#999');
+		restartGameText.textAlign = 'center';
+		restartGameText.x = canvas.width * 0.5;
+		restartGameText.y = canvas.height * 0.5 + 20;
+		
+		stage.addChild(gameOverText, restartGameText);
+		
+		$(document).bind('keyup.restart', restartGame);
+		
+		stage.update();
+	}
+	
+	function restartGame(e) {
+		if (e.keyCode == 32) {
+			stage.removeChild(gameOverText, restartGameText);
+			gameOverText = null;
+			restartGameText = null;
+			stage.update();
+			
+			startGame();
+			$(document).unbind('keyup.restart', restartGame);
+		}
+	}
+	
 	function onStageTick(e) {
-		/* Handle triangle movement */
-		if (moveRight) {
-			if (triangle.x >= canvas.width -5) {
-				triangle.x = canvas.width -5;
-			} else {
-				triangle.x += 5;
+		if (gameState == 'play') {
+			/* Life reaches 0% or squares are exploded, the game ends */
+			if (playerLife == 0 || squares.length == 0) {
+				gameOver();
+				return;
 			}
 			
-		} else if (moveLeft) {
-			if (triangle.x <= 5) {
-				triangle.x = 5;
-			} else {
-				triangle.x -= 5;
-			}
-		}
-		
-		/* Handle the shooting animation */
-		if (bullets.length != 0) {
-			for (var i = 0; i < bullets.length; i++) {
-				var currentBullet = bullets[i];
-				currentBullet.y -= 5;
+			/* Handle triangle movement */
+			if (moveRight) {
+				if (triangle.x >= canvas.width -5) {
+					triangle.x = canvas.width -5;
+				} else {
+					triangle.x += 5;
+				}
 				
-				if (currentBullet.y < 0) {
-					stage.removeChild(currentBullet);
-					bullets.splice(i, 1);
+			} else if (moveLeft) {
+				if (triangle.x <= 5) {
+					triangle.x = 5;
+				} else {
+					triangle.x -= 5;
 				}
 			}
-		}
-		
-		/* Collisions: squares with Bullets */
-		if (bullets.length != 0) {
-			for (var bullet = 0; bullet < bullets.length; bullet++) {
-				for (var square = 0; square < squares.length; square++) {
-					var aBullet = bullets[bullet];
-					var aSquare = squares[square];
-					var dx = aSquare.x - aBullet.x;
-					var dy = aSquare.y - aBullet.y;
-					var distance = Math.sqrt(dx * dx + dy * dy);
+			
+			/* Handle the shooting animation */
+			if (bullets.length != 0) {
+				for (var i = 0; i < bullets.length; i++) {
+					var currentBullet = bullets[i];
+					currentBullet.y -= 5;
 					
-					if (distance < 2.5 + 2.5) {
-						stage.removeChild(aBullet);
-						bullets.splice(bullet, 1);
+					if (currentBullet.y < 0) {
+						stage.removeChild(currentBullet);
+						bullets.splice(i, 1);
+					}
+				}
+			}
+			
+			/* Collisions: squares with Bullets */
+			if (bullets.length != 0) {
+				for (var bullet = 0; bullet < bullets.length; bullet++) {
+					for (var square = 0; square < squares.length; square++) {
+						var aBullet = bullets[bullet];
+						var aSquare = squares[square];
+						var dx = aSquare.x - aBullet.x;
+						var dy = aSquare.y - aBullet.y;
+						var distance = Math.sqrt(dx * dx + dy * dy);
 						
-						if (!aSquare.isAnimated) {
-							aSquare.isAnimated = true;
-						} else {
-							stage.removeChild(aSquare);
-							squares.splice(square, 1);
+						if (distance < 2.5 + 2.5) {
+							stage.removeChild(aBullet);
+							bullets.splice(bullet, 1);
 							
-							gameScore += 5;
-							scoreText.text = 'Score: ' + gameScore + 'pts';
-						} 
-						
-						stage.update();
-						return;
+							if (!aSquare.isAnimated) {
+								aSquare.isAnimated = true;
+							} else {
+								stage.removeChild(aSquare);
+								squares.splice(square, 1);
+								
+								gameScore += 5;
+								scoreText.text = 'Score: ' + gameScore + 'pts';
+							} 
+							
+							stage.update();
+							return;
+						}
 					}
 				}
 			}
-		}
-		
-		/* Collisions: squares with the triangle */
-		if (squares.length != 0) {
-			for (var singleSquare = 0; singleSquare < squares.length; singleSquare++) {
-				var theSquare = squares[singleSquare];
-				
-				if (theSquare.y - (theSquare.height * 0.5) > triangle.y) {
-					if (theSquare.x <= triangle.x + (triangle.width * 0.5) && theSquare.x >= triangle.x - (triangle.width * 0.5)) {
-						//console.log('hit triangle');
-						stage.removeChild(theSquare);
-						squares.splice(singleSquare, 1);
-						
-						playerLife -= 5;
-						lifeText.text = 'Life: ' + playerLife + '%';
-						
-						stage.update();
-						return;
-					}
-				}
-			}
-		}
-		
-		/* Animate those squares that have been shot down */
-		if (squares.length != 0) {
-			for (var animSquare = 0; animSquare < squares.length; animSquare++) {
-				var animatedSquare = squares[animSquare];
-				
-				if (animatedSquare.isAnimated) {
-					animatedSquare.y += 5;
+			
+			/* Collisions: squares with the triangle */
+			if (squares.length != 0) {
+				for (var singleSquare = 0; singleSquare < squares.length; singleSquare++) {
+					var theSquare = squares[singleSquare];
 					
-					if (animatedSquare.y > canvas.height) {
-						stage.removeChild(animatedSquare);
-						squares.splice(animSquare, 1);
+					if (theSquare.y - (theSquare.height * 0.5) > triangle.y) {
+						if (theSquare.x <= triangle.x + (triangle.width * 0.5) && theSquare.x >= triangle.x - (triangle.width * 0.5)) {
+							//console.log('hit triangle');
+							stage.removeChild(theSquare);
+							squares.splice(singleSquare, 1);
+							
+							playerLife -= 5;
+							lifeText.text = 'Life: ' + playerLife + '%';
+							
+							stage.update();
+							return;
+						}
+					}
+				}
+			}
+			
+			/* Animate those squares that have been shot down */
+			if (squares.length != 0) {
+				for (var animSquare = 0; animSquare < squares.length; animSquare++) {
+					var animatedSquare = squares[animSquare];
+					
+					if (animatedSquare.isAnimated) {
+						animatedSquare.y += 5;
+						
+						if (animatedSquare.y > canvas.height) {
+							stage.removeChild(animatedSquare);
+							squares.splice(animSquare, 1);
+						}
 					}
 				}
 			}
